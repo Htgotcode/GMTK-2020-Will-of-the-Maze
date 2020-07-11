b5cef591469c35ceb81ecb159ca7c2d6366e7185
Kyle van Antwerpen <kyle.vanantwerpen@gmail.com>
2020-07-11 02:13:29 +0200

    Movement and Fog of War v1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,33 +7,47 @@ public class PlayerController : MonoBehaviour
 
     private float xMove = -1.5f;
     private float yMove = -0.5f;
-
-
+    LayerMask layerWalls = 1 << 8;
     private Rigidbody2D rb;
+    RaycastHit2D hit;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update() {
         if (Input.GetKeyDown(KeyCode.W)) {
-            yMove += 1;
+            hit = Physics2D.Raycast(transform.position, Vector2.up, 0.5f, layerWalls);
+            if (hit.collider == null) {
+                yMove += 1;
+                transform.rotation = Quaternion.Euler(0,0, -90);
+            }
         }
         if (Input.GetKeyDown(KeyCode.A)) {
-            xMove -= 1;
+            hit = Physics2D.Raycast(transform.position, Vector2.left, 0.5f, layerWalls);
+            if (hit.collider == null) {
+                xMove -= 1;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
         }
         if (Input.GetKeyDown(KeyCode.S)) {
-            yMove -= 1;
+            hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, layerWalls);
+            if (hit.collider == null) {
+                yMove -= 1;
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
         }
         if (Input.GetKeyDown(KeyCode.D)) {
-            xMove += 1;
+            hit = Physics2D.Raycast(transform.position, Vector2.right, 0.5f, layerWalls);
+            if (hit.collider == null) {
+                xMove += 1;
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+            }
         }
 
         rb.MovePosition(new Vector2(xMove, yMove));
     }
 }
+
